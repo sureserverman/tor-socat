3a0e1ee49f42ff55f74cc4a40fddd84df3d00aa6
Sure Serverman <host.unmixable219@passinbox.com>
2024-04-22 20:16:43 +0100

    Updated actions and added BRIDGED var

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-tor Bridge "$BRIDGE1" Bridge "$BRIDGE2" &
+if [ $BRIDGED = "Y" ]; then
+    tor Bridge "$BRIDGE1" Bridge "$BRIDGE2" &
+else
+    tor &
+fi
 socat TCP4-LISTEN:${PORT},reuseaddr,fork SOCKS4A:127.0.0.1:dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion:${PORT},socksport=9050
