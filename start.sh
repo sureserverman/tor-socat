a8d8779be4cdd68f82acd30e416c121aaad112b9
Sure Serverman <dupastiy@proton.me>
2026-01-30 16:53:44 +0000

    Rollback to hardcoded bridges

--- start.sh
+++ start.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 
-BRIDGE_URL="https://bridges.torproject.org/bridges?transport=obfs4"
-BRIDGE_COUNT="${BRIDGE_COUNT:-2}"
+# BRIDGE_URL="https://bridges.torproject.org/bridges?transport=obfs4"
+# BRIDGE_COUNT="${BRIDGE_COUNT:-2}"
+# 
+# fetch_bridges() {
+  # curl -fsSL "https://bridges.torproject.org/bridges?transport=obfs4" \
+    # | sed -E 's/<br[[:space:]]*\/?>/\n/gI' \
+    # | sed -E 's/<[^>]+>//g' \
+    # | sed -E 's/&#43;/+/g; s/&amp;/\&/g; s/&#x2[Bb];/+/g' \
+    # | awk '
+        # /^[[:space:]]*obfs4 / {
+          # gsub(/^[[:space:]]+/, "");
+          # gsub(/[[:space:]]+$/, "");
+          # print
+        # }
+      # ' \
+    # | head -n "${BRIDGE_COUNT:-2}"
+# }
+# 
+# BRIDGES="$(fetch_bridges || true)"
+# 
+# if [ -z "${BRIDGES}" ]; then
+  # echo "ERROR: could not fetch obfs4 bridges from $BRIDGE_URL"
+  # exit 1
+# fi
+# BRIDGE1="$(echo "${BRIDGES}" | sed -n '1p')"
+# BRIDGE2="$(echo "${BRIDGES}" | sed -n '2p')"
 
-fetch_bridges() {
-  curl -fsSL "https://bridges.torproject.org/bridges?transport=obfs4" \
-    | sed -E 's/<br[[:space:]]*\/?>/\n/gI' \
-    | sed -E 's/<[^>]+>//g' \
-    | sed -E 's/&#43;/+/g; s/&amp;/\&/g; s/&#x2[Bb];/+/g' \
-    | awk '
-        /^[[:space:]]*obfs4 / {
-          gsub(/^[[:space:]]+/, "");
-          gsub(/[[:space:]]+$/, "");
-          print
-        }
-      ' \
-    | head -n "${BRIDGE_COUNT:-2}"
-}
-
-BRIDGES="$(fetch_bridges || true)"
-
-if [ -z "${BRIDGES}" ]; then
-  echo "ERROR: could not fetch obfs4 bridges from $BRIDGE_URL"
-  exit 1
-fi
-BRIDGE1="$(echo "${BRIDGES}" | sed -n '1p')"
-BRIDGE2="$(echo "${BRIDGES}" | sed -n '2p')"
+BRIDGE1="obfs4 159.195.56.139:53785 80AF3EEDD08BE7C86779E9D9ABBED7FCACCE8CBB cert=uTKcPUtqbb+PStmG4XXzKZxd4Y1Pl0fvirpmcBaQphtdIY9/CmKF+gQIN/jVhZUPZ+XaTQ iat-mode=0"
+BRIDGE2="obfs4 185.232.71.49:43173 3F4CA17BCA6E1318A982D2672DCE4F6E66404015 cert=xemMtBcAlH0N3vNuwJhJMXcY+sV7/JcEXm6LY9lfsX21pjDkg91azC3SVNi1MUC6blmxXg iat-mode=0"
 
 tor Bridge "$BRIDGE1" Bridge "$BRIDGE2" &
 
